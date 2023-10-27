ddac310f338bf9876d7b089124e13f1fdfb92a34
Thalles Freitas <thalles@studiowox.com.br>
2023-10-27 15:33:32 -0300

    teste job apply 2

--- src/modules/slug/slug.serializer.ts
+++ src/modules/slug/slug.serializer.ts
@@ -1,4 +1,3 @@
-/* eslint-disable indent */
 import {
   Candidate,
   Freelancer,
@@ -70,14 +69,13 @@ export const getBySlug = async (
     case 'job':
       data = payloadType.data as JobComplete;
 
-      const owner = payloadType.userID == data.updatedById?.id;
+      const owner = payloadType.userID === data.updatedById?.id;
       let jobApplied = false;
 
       data.jobApplication?.map((item: any) => {
         if (item.id === payloadType.userID) jobApplied = true;
         return false;
       });
-
       response = {
         type: payloadType.type,
         id: data.id,
@@ -115,18 +113,49 @@ export const getBySlug = async (
     default:
       break;
   }
-  console.log('response');
-  console.log('response');
-  console.log('response');
-  console.log('response');
-  console.log('response');
-  console.log('response');
-  console.log('response');
-  console.log('response');
-  console.log('response');
-  console.log('response');
-  console.log('response');
-  console.log('response');
-  console.log(response);
+  return response;
+};
+export const getBySlugApplication = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+  payload: unknown
+) => {
+  console.log('getBySlugApplication');
+  console.log('payload');
+  console.log(payload);
+  const payloadType = payload as PayloadType;
+  let response;
+  let data;
+
+  switch (payloadType.type) {
+    case 'user':
+      data = payloadType.data as UserComplete;
+      console.log('data user');
+      console.log(data);
+      console.log(data?.createdJobApplication);
+      response = {
+        type: payloadType.type,
+        createdJobApplication: data?.createdJobApplication,
+        id: data?.id,
+        // stripe_id: data?.stripe_id,
+        optin: data?.optin,
+        email: data?.email,
+        name: data?.name,
+        phone: data?.phone,
+        slug: data?.slug,
+        isActive: data?.isActive,
+        createdAt: data?.createdAt,
+        updatedAt: data?.updatedAt,
+        teste: '3',
+        candidate: data?.candidate,
+        serviceProvider: data?.serviceProvider,
+        freelancer: data?.freelancer,
+      };
+      break;
+
+    default:
+      break;
+  }
+
   return response;
 };
